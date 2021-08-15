84d0884dc5a82395bbe86e395140cb52f4d3dc78
Prasanna <dev3.getinfy@gmail.com>
2021-08-15 21:56:46 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import Home from './HomeScreen/home'
+function App() {
+  return (
+    <div className="App">
+      <header>
+        <Home/>
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/HomeScreen/home.js
@@ -0,0 +1,15 @@
+import React, {Component} from 'react'
+import Nav from '../Components/Navgation/Nav'
+class home extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {  };
+    }
+    render() {
+        return (
+            <Nav/>
+        );
+    }
+}
+
+export default home;
